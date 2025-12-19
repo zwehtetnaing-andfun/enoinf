b05e9b9c0bc187a5b818e33838ea25d1e205524d
Your Name <you@example.com>
2025-12-19 11:37:19 +0800

    css2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,10 +30,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <style>
-   div.pageVisualWrapper {
-    padding: 62px 0 1em 2em;
+     div.pageVisualWrapper {
+  padding: 42px 0 1em 2em;
     position: relative;
-     }
+}
         </style>
     @stack('styles')
 </head>

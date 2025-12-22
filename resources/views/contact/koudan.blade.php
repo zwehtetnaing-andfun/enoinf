ca10fb1f7996dc7ba69f8ca7159e53683a264801
Zwe Htet <zwehtetnaing@andfun.biz>
2025-12-22 14:19:28 +0630

    fix kata and select

--- resources/views/contact/koudan.blade.php
+++ resources/views/contact/koudan.blade.php
@@ -40,13 +40,10 @@
                             <label class="col-md-4 control-label" for="type">*企業 or 個人</label>
                             <div class="col-md-4">
                                 <select name="type" class="form-control" id="type" required>
-                                    <option value="">選択してください</option>
-                                    <option value="企業">企業</option>
-                                    <option value="個人">個人</option>
+                                    <option value="" selected>選択してください</option>
+                                    <option value="企業" {{ old('type') === '企業' ? 'selected' : '' }}>企業</option>
+                                    <option value="個人" {{ old('type') === '個人' ? 'selected' : '' }}>個人</option>
                                 </select>
-                                <script>
-                                    document.getElementById('type').value = @json(old('type'));
-                                </script>
                             </div>
                         </div>
                         
@@ -103,63 +100,61 @@
                             <label class="col-md-4 control-label" for="pref">*都道府県</label>
                             <div class="col-md-4">
                                 <select name="pref" class="form-control" id="pref" required>
-                                    <option value="">選択してください</option>
-                                    <option value="北海道">北海道</option>
-                                    <option value="青森県">青森県</option>
-                                    <option value="岩手県">岩手県</option>
-                                    <option value="宮城県">宮城県</option>
-                                    <option value="秋田県">秋田県</option>
-                                    <option value="山形県">山形県</option>
-                                    <option value="福島県">福島県</option>
-                                    <option value="茨城県">茨城県</option>
-                                    <option value="栃木県">栃木県</option>
-                                    <option value="群馬県">群馬県</option>
-                                    <option value="埼玉県">埼玉県</option>
-                                    <option value="千葉県">千葉県</option>
-                                    <option value="東京都">東京都</option>
-                                    <option value="神奈川県">神奈川県</option>
-                                    <option value="新潟県">新潟県</option>
-                                    <option value="富山県">富山県</option>
-                                    <option value="石川県">石川県</option>
-                                    <option value="福井県">福井県</option>
-                                    <option value="山梨県">山梨県</option>
-                                    <option value="長野県">長野県</option>
-                                    <option value="岐阜県">岐阜県</option>
-                                    <option value="静岡県">静岡県</option>
-                                    <option value="愛知県">愛知県</option>
-                                    <option value="三重県">三重県</option>
-                                    <option value="滋賀県">滋賀県</option>
-                                    <option value="京都府">京都府</option>
-                                    <option value="大阪府">大阪府</option>
-                                    <option value="兵庫県">兵庫県</option>
-                                    <option value="奈良県">奈良県</option>
-                                    <option value="和歌山県">和歌山県</option>
-                                    <option value="鳥取県">鳥取県</option>
-                                    <option value="島根県">島根県</option>
-                                    <option value="岡山県">岡山県</option>
-                                    <option value="広島県">広島県</option>
-                                    <option value="山口県">山口県</option>
-                                    <option value="徳島県">徳島県</option>
-                                    <option value="香川県">香川県</option>
-                                    <option value="愛媛県">愛媛県</option>
-                                    <option value="高知県">高知県</option>
-                                    <option value="福岡県">福岡県</option>
-                                    <option value="佐賀県">佐賀県</option>
-                                    <option value="長崎県">長崎県</option>
-                                    <option value="熊本県">熊本県</option>
-                                    <option value="大分県">大分県</option>
-                                    <option value="宮崎県">宮崎県</option>
-                                    <option value="鹿児島県">鹿児島県</option>
-                                    <option value="沖縄県">沖縄県</option>
-                                    <option value="海外">海外</option>
-                                </select>@error('pref')
+                                    <option value="" selected>選択してください</option>
+                                    <option value="北海道" {{ old('pref') === '北海道' ? 'selected' : '' }}>北海道</option>
+                                    <option value="青森県" {{ old('pref') === '青森県' ? 'selected' : '' }}>青森県</option>
+                                    <option value="岩手県" {{ old('pref') === '岩手県' ? 'selected' : '' }}>岩手県</option>
+                                    <option value="宮城県" {{ old('pref') === '宮城県' ? 'selected' : '' }}>宮城県</option>
+                                    <option value="秋田県" {{ old('pref') === '秋田県' ? 'selected' : '' }}>秋田県</option>
+                                    <option value="山形県" {{ old('pref') === '山形県' ? 'selected' : '' }}>山形県</option>
+                                    <option value="福島県" {{ old('pref') === '福島県' ? 'selected' : '' }}>福島県</option>
+                                    <option value="茨城県" {{ old('pref') === '茨城県' ? 'selected' : '' }}>茨城県</option>
+                                    <option value="栃木県" {{ old('pref') === '栃木県' ? 'selected' : '' }}>栃木県</option>
+                                    <option value="群馬県" {{ old('pref') === '群馬県' ? 'selected' : '' }}>群馬県</option>
+                                    <option value="埼玉県" {{ old('pref') === '埼玉県' ? 'selected' : '' }}>埼玉県</option>
+                                    <option value="千葉県" {{ old('pref') === '千葉県' ? 'selected' : '' }}>千葉県</option>
+                                    <option value="東京都" {{ old('pref') === '東京都' ? 'selected' : '' }}>東京都</option>
+                                    <option value="神奈川県" {{ old('pref') === '神奈川県' ? 'selected' : '' }}>神奈川県</option>
+                                    <option value="新潟県" {{ old('pref') === '新潟県' ? 'selected' : '' }}>新潟県</option>
+                                    <option value="富山県" {{ old('pref') === '富山県' ? 'selected' : '' }}>富山県</option>
+                                    <option value="石川県" {{ old('pref') === '石川県' ? 'selected' : '' }}>石川県</option>
+                                    <option value="福井県" {{ old('pref') === '福井県' ? 'selected' : '' }}>福井県</option>
+                                    <option value="山梨県" {{ old('pref') === '山梨県' ? 'selected' : '' }}>山梨県</option>
+                                    <option value="長野県" {{ old('pref') === '長野県' ? 'selected' : '' }}>長野県</option>
+                                    <option value="岐阜県" {{ old('pref') === '岐阜県' ? 'selected' : '' }}>岐阜県</option>
+                                    <option value="静岡県" {{ old('pref') === '静岡県' ? 'selected' : '' }}>静岡県</option>
+                                    <option value="愛知県" {{ old('pref') === '愛知県' ? 'selected' : '' }}>愛知県</option>
+                                    <option value="三重県" {{ old('pref') === '三重県' ? 'selected' : '' }}>三重県</option>
+                                    <option value="滋賀県" {{ old('pref') === '滋賀県' ? 'selected' : '' }}>滋賀県</option>
+                                    <option value="京都府" {{ old('pref') === '京都府' ? 'selected' : '' }}>京都府</option>
+                                    <option value="大阪府" {{ old('pref') === '大阪府' ? 'selected' : '' }}>大阪府</option>
+                                    <option value="兵庫県" {{ old('pref') === '兵庫県' ? 'selected' : '' }}>兵庫県</option>
+                                    <option value="奈良県" {{ old('pref') === '奈良県' ? 'selected' : '' }}>奈良県</option>
+                                    <option value="和歌山県" {{ old('pref') === '和歌山県' ? 'selected' : '' }}>和歌山県</option>
+                                    <option value="鳥取県" {{ old('pref') === '鳥取県' ? 'selected' : '' }}>鳥取県</option>
+                                    <option value="島根県" {{ old('pref') === '島根県' ? 'selected' : '' }}>島根県</option>
+                                    <option value="岡山県" {{ old('pref') === '岡山県' ? 'selected' : '' }}>岡山県</option>
+                                    <option value="広島県" {{ old('pref') === '広島県' ? 'selected' : '' }}>広島県</option>
+                                    <option value="山口県" {{ old('pref') === '山口県' ? 'selected' : '' }}>山口県</option>
+                                    <option value="徳島県" {{ old('pref') === '徳島県' ? 'selected' : '' }}>徳島県</option>
+                                    <option value="香川県" {{ old('pref') === '香川県' ? 'selected' : '' }}>香川県</option>
+                                    <option value="愛媛県" {{ old('pref') === '愛媛県' ? 'selected' : '' }}>愛媛県</option>
+                                    <option value="高知県" {{ old('pref') === '高知県' ? 'selected' : '' }}>高知県</option>
+                                    <option value="福岡県" {{ old('pref') === '福岡県' ? 'selected' : '' }}>福岡県</option>
+                                    <option value="佐賀県" {{ old('pref') === '佐賀県' ? 'selected' : '' }}>佐賀県</option>
+                                    <option value="長崎県" {{ old('pref') === '長崎県' ? 'selected' : '' }}>長崎県</option>
+                                    <option value="熊本県" {{ old('pref') === '熊本県' ? 'selected' : '' }}>熊本県</option>
+                                    <option value="大分県" {{ old('pref') === '大分県' ? 'selected' : '' }}>大分県</option>
+                                    <option value="宮崎県" {{ old('pref') === '宮崎県' ? 'selected' : '' }}>宮崎県</option>
+                                    <option value="鹿児島県" {{ old('pref') === '鹿児島県' ? 'selected' : '' }}>鹿児島県</option>
+                                    <option value="沖縄県" {{ old('pref') === '沖縄県' ? 'selected' : '' }}>沖縄県</option>
+                                    <option value="海外" {{ old('pref') === '海外' ? 'selected' : '' }}>海外</option>
+                                </select>
+                                @error('pref')
                                     <p class="text-danger">{{ $message }}</p>
-                                @enderror<br>
+                                @enderror
+                                <br>
                                 <span class="help-block">海外の方は「海外」を選択してください</span>
-                              
-                                <script>
-                                    document.getElementById('pref').value = @json(old('pref'));
-                                </script>
                             </div>
                         </div>
                         
@@ -194,6 +189,9 @@
                             <label class="col-md-4 control-label" for="fax">FAX番号</label>
                             <div class="col-md-4">
                                 <input type="text" name="fax" class="form-control input-md" id="fax" value="{{ old('fax') }}" />
+                                @error('fax')
+                                    <p class="text-danger">{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                         
